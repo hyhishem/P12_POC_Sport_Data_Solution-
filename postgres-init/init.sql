ad8eeda998babf2e5a8bc58d032a91d51e1fa88c
hyhishem <001data2025@gmail.com>
2026-05-30 03:02:38 +0200

    services_docker

---
+++ postgres-init/init.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA IF NOT EXISTS strava;
+
+CREATE TABLE IF NOT EXISTS strava.sport (
+    id UUID PRIMARY KEY,
+    id_salarie INT,
+    nom TEXT,
+    prenom TEXT,
+    type_sport TEXT,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    duration_minutes INT,
+    distance_m INT
+);
+
+CREATE SCHEMA IF NOT EXISTS BI;
+
+
+CREATE TABLE IF NOT EXISTS BI.prime_sport (
+    id_salarie          INT PRIMARY KEY,
+    nom                 TEXT,
+    prenom              TEXT,
+    nb_activites        INT,
+    moyen_de_deplacement TEXT    
+    salaire_brut        NUMERIC,
+    prime               NUMERIC,
+    eligible_avantage_prime      BOOLEAN,
+    eligible_avantage_5jours       BOOLEAN,
+);
